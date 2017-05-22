93eb43fa6b01ab838d3705bd4eb15b5ab6a7a4ea
zluo16 <zluo@alumni.berklee.edu>
2017-05-22 17:48:15 -0400

    created bear database

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 26, "M", "brown", "smooth and creamy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 13, "M", "black", "what do you think?", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 113, "F", "brown", "calm", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 24, "M", "brown", "stoic", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 22, "F", "brown", "sweet", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 38, "M", "black", "he's a mean one", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 56, "F", "black", "nice", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 14, "M", "brown", "uneasy", 0);
